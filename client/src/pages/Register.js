cf00e8c44aa89360a3cf5293e86ee2ab6100fb9f
b1bin <bobo692233@gmail.com>
2024-02-03 01:31:50 +0200

    Made the registration for the User

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,17 +1,64 @@
 // Register.js
 
 import React, { useState } from 'react';
-
+import { Navigate } from 'react-router-dom';
 const Register = () => {
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [navigate, setNavigate] = useState(false);
+  const [error, setError] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Handle register logic here
+    // Form validation
+    if (!username || !password) {
+      setError('Username and password are required.');
+      return;
+    };
+    if (!confirmPassword){
+      setError('You need to confirm your password!')
+      return;
+    };
+    if (password !== confirmPassword){
+      setError('Your passwords need to match!')
+      return;
+    };
+    if (password.length < 8) {
+      setError('Password must be at least 8 characters long.');
+      return;
+    };
+    
+    try {
+      const response = await fetch('http://localhost:5000/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      if (!response.ok) {
+        console.error('Registration failed:', response.statusText);
+        return;
+      }
+
+      console.log('Registration successful!');
+      setNavigate(true);
+
+    } catch (error) {
+      console.error('Error during registration:', error);
+    }
     console.log('Registering with:', username, password, confirmPassword);
   };
+
+  if (navigate) {
+    return <Navigate to="/" />;
+  }
 
   return (
     <div id="container">
@@ -40,12 +87,13 @@ const Register = () => {
         <input
           type="password"
           name="confirmPassword"
-          id="confirm"
+          id="confirmPassword"
           placeholder="Confirm Password"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
         />
 
+        {error && <p style={{ color: 'red' }}>{error}</p>}
         <input type="submit" name="button" value="Register" id="input" />
       </form>
     </div>
